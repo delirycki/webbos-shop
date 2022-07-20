f38f3d2ac2242d2dd4664960c60fb74d496b5d75
Paweł Komarnicki <delirycki@gmail.com>
2022-07-20 16:12:01 +0200

    after cache

---
+++ front/pages/products/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+function ProductsPage() {
+  const router = useRouter();
+  const page = parseInt(router.query.page);
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products page={page || 1} />;
+      <Pagination page={page || 1} />;
+    </div>
+  );
+}
+
+export default ProductsPage;
